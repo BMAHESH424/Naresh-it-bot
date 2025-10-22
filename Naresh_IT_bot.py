7656c0c5990b8c4ed34f1f1ab000c5b3636fc7b1
BMAHESH424 <145210040+BMAHESH424@users.noreply.github.com>
2025-10-23 00:30:20 +0530

    Naresh_IT_bot.py

--- Naresh_IT_bot.py
+++ Naresh_IT_bot.py
@@ -10,8 +10,8 @@ from dotenv import load_dotenv
 # Import necessary Google/Gemini components from LangChain
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 import asyncio
@@ -1243,3 +1243,4 @@ st.markdown(f'''
   </a>
 </div>
 ''', unsafe_allow_html=True)
+
